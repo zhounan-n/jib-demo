8080941e9acbbd7af67ba056f672995153e9e091
zhounan <zhounan@zhuangdianwenhua.com>
2019-04-18 14:23:50 +0800

    update

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * Created by zhoun on 2019-04-17
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("");
+    }
+
+}
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zhoun on 2019-04-17
+ */
+@Controller
+public class TestController {
+
+
+
+    public String hello(){
+        return "hello";
+    }
+}
